c5ad6ccc91a4f94098b441b11abf86198ac4d4cf
jihyunhillpark <girl9505@naver.com>
2023-10-02 02:31:43 +0900

    task7: prop적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Hello from './component/Hello';
 
-import styles from "./App.module.css";
-
 function App() {
   return (
     <div className="App">
-      <div className={styles.box}>App</div>
-      <Hello></Hello>
+      <Hello age={10}></Hello>
+      <Hello age={20}></Hello>
+      <Hello age={30}></Hello>
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,26 +1,24 @@
-export default function Hello() {
+import {useState} from "react";
+import UserName from "./UserName";
 
-    function showName() {
-        console.log("Mike");
-    }
-    function showAge(age) {
-        console.log(age);
-    }
-    function showText(txt) {
-        console.log(txt);
-    }
+export default function Hello({age}) {
+    // let name = "Mike";
+    const [name, setName] = useState('Mike');
+    const msg = age > 19? "성인 입니다." : "미성년자입니다.";
+    // function changename() {
+    //     const newName = name === "Mike"? "Jane" : "Mike";
+    //     // document.getElementById("name").innerText = name;
+    //     setName(newName);
+    // }
+
     return (
         <div>
-            <h1>Hello</h1>
-            <button onClick={showName}>Show name</button>
+            <h2 id="name">{name} ({age}) : {msg}</h2>
+            <UserName name={name}></UserName>
             <button onClick={() => {
-                showAge(30);
-            }}>Show age</button>
-            <input type="text" onChange={e => {
-                const txt = e.target.value;
-                showText(txt);
-            }}/>
+                setName(name === "Mike"? "Jane" : "Mike");
+            }}>Change</button>
         </div>
-    )
+    );
 }
 
