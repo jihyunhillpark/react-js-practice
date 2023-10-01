9e1351eca831ae75b8d4789184ec923907b605b0
jihyunhillpark <girl9505@naver.com>
2023-10-02 03:27:00 +0900

    lesson10 : 라우터 구현 (react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Day from "./component/Day";
 import DayList from "./component/DayList";
+import EmptyPage from "./component/EmptyPage";
 import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Header />
-    <DayList />
-    <Day />
-  </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList/>}/>
+          <Route path="/day/:day" element={<Day/>}/>
+          <Route path="*" element={<EmptyPage/>}></Route>
+        </Routes>        
+      </div>
+    </BrowserRouter>);
 }
 
 export default App;
