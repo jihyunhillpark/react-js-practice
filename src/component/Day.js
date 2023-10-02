f0f08f7d27c52774d81bc2dcda9a093a075e2328
jihyunhillpark <girl9505@naver.com>
2023-10-02 11:37:43 +0900

    lesson113 : custom hook 만들기

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,21 +1,10 @@
 import { useParams } from "react-router-dom";
 import Word from "./Word";
-import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
     const { day } = useParams();
-    // const wordList = dummy.words.filter(word => word.day === Number(day));
-    const [words, setWords] = useState([]);
-
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setWords(data);
-            });
-    }, [day]);
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
     
     return <>
         <h2>Day {day}</h2>
